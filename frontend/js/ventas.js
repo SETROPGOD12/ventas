253e52edf6d4af064134518a03a07e05051782ca
SETROPGOD12 <andresfeliportez@gmail.com>
2025-05-23 08:53:14 -0500

    se agrega el venta.js

---
+++ frontend/js/ventas.js
@@ -0,0 +1,62 @@
+const API_CLIENTES = "http://localhost:3000/api/clientes";
+const API_PRODUCTOS = "http://localhost:3000/api/productos";
+const API_VENTAS = "http://localhost:3000/api/ventas";
+
+document.getElementById('formVenta').addEventListener('submit', async(e) => {
+    e.preventDefault();
+    const id_cliente = document.getElementById('clientes').value;
+     const id_producto = document.getElementById('productos').value;
+      const cantidad = document.getElementById('cantidad').value;
+
+      const res = await fetch(API_VENTAS,{
+        method: 'POST',
+        headers: {'content-Type': 'application/json'},
+        body: JSON.stringify({id_cliente, id_producto, cantidad})
+        });
+        const data = await res.json();
+        if (res.ok) {
+            alert(data.mensaje);
+            document.getElementById('formVenta'.reset())
+            cargarVentas();
+        }else{
+            alert('Error: '+ data.error);
+        }
+});
+
+async function cargarClientesYProductos() {
+    const clientes = await(await fetch(API_CLIENTES)).json();
+    const productos = await(await fetch(API_PRODUCTOS)).json();
+    
+    const clienteSelect = document.getElementById('clientes');
+    clienteSelect.innerHTML = '<option value=""> Seleccione cliente </option>';
+    clientes.forEach(c =>{
+        clienteSelect.innerHTML += `<option value="${c.id}">${p.nombre} </option>`;
+    });
+     const productoSelect = document.getElementById('productos');
+    productoSelectSelect.innerHTML = '<option value=""> Seleccione producto </option>';
+    productos.forEach(c =>{
+        productoSelectSelect.innerHTML += `<option value="${p.id}">${p.nombre_prod} (stock: ${p.stock})</option>`;
+});
+
+}
+
+async function cargarVentas() {
+    const ventas = await(await fetch(API_VENTAS)).json();
+    const tabla = document.getElementById('tablaVentas');
+    tabla.innerHTML = '';
+    ventas.forEach(v => {
+        tabla.innerHTML += `
+        <tr>
+            <td>${v.id}</td>
+            <td>${v.clientes}</td>
+            <td>${v.productos}</td>
+            <td>${v.cantidad}</td>
+            <td>${v.precio_unitario}</td>
+            <td>${v.total}</td>
+            <td>${new data(v.fecha).toLocaleString()}</td>
+        </tr>`;
+    });
+    
+}
+cargarClientesYProductos();
+cargarVentas();
